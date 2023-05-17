7330b54df3975d289dd9649aead52730cd4f3754
Sara Melissa Marroquín Vega <sara.marroquin@inducascos.com>
2023-05-17 07:11:49 -0500

    initial load

---
+++ TaskManager/TaskManager/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Entities
+{
+    public class Entity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? CreatedDate { get; set; } /// <summary>
+        /// el signo de interrogación significa que el campo puede ser nuleable
+        /// </summary>
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ TaskManager/TaskManager/Entities/Group.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskManager.Entities
+{
+    public class Group : Entity
+    {
+        public string Description { get; set; }
+    }
+}
