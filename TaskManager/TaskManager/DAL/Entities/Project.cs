f9c42b05f5704db5bcadf18e1132c02a4311094a
Heider98 <eider820hotmail.com>
2023-05-24 16:23:11 -0500

    creation of the projects entity

---
+++ TaskManager/TaskManager/DAL/Entities/Project.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.DAL.Entities
+{
+    public class Project : Entity
+    {
+        [Display(Name = "Nombre del proyecto")]
+        [MaxLength(45, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Descripcion proyecto")]
+        [MaxLength(45, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Description { get; set; }
+
+        [Display(Name = "Id usuario")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public Guid IdUser { get; set; }
+
+    }
+}
